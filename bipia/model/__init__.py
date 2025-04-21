2b98a37ecfa6840628ee3b86e0e83abd269282d3
yang <yang@example.com>
2025-04-21 16:47:06 +0800

    删除多余配置文件

--- bipia/model/__init__.py
+++ bipia/model/__init__.py
@@ -7,19 +7,6 @@ from collections import OrderedDict
 from accelerate.logging import get_logger
 
 from .gpt import GPT35, GPT4, GPT35WOSystem, GPT4WOSystem
-from .llama import (
-    Alpaca,
-    Vicuna,
-    Baize,
-    StableVicuna,
-    Koala,
-    GPT4ALL,
-    Wizard,
-    Guanaco,
-    Llama2,
-)
-from .vllm_worker import Dolly, StableLM, MPT, Mistral
-from .llm_worker import RwkvModel, OASST, ChatGLM, FastChatT5
 
 logger = get_logger(__name__)
 
@@ -29,23 +16,6 @@ LLM_NAME_TO_CLASS = OrderedDict(
         ("gpt4", GPT4),
         ("gpt35_wosys", GPT35WOSystem),
         ("gpt4_wosys", GPT4WOSystem),
-        ("alpaca", Alpaca),
-        ("vicuna", Vicuna),
-        ("baize", Baize),
-        ("stablelm", StableLM),
-        ("stablevicuna", StableVicuna),
-        ("dolly", Dolly),
-        ("rwkv", RwkvModel),
-        ("oasst", OASST),
-        ("chatglm", ChatGLM),
-        ("koala", Koala),
-        ("mpt", MPT),
-        ("t5", FastChatT5),
-        ("gpt4all", GPT4ALL),
-        ("wizard", Wizard),
-        ("guanaco", Guanaco),
-        ("llama2", Llama2),
-        ("mistral", Mistral),
     ]
 )
 
