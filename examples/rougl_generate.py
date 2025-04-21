d293840fec0a84572a295f0a953cecee13bafd5f
yang <yang@example.com>
2025-04-21 17:38:12 +0800

    generate

---
+++ examples/rougl_generate.py
@@ -0,0 +1,54 @@
+import os
+import yaml
+import multiprocessing
+
+
+"""
+    这里默认将生成模型放于config/gpt35.yaml里面
+    qa任务无法获取数据暂时不做
+"""
+
+
+def generate(task):
+    seed = 2023
+    file_path = os.path.dirname(__file__)
+    home_path = os.path.dirname(file_path)
+    llm_config_file = f"{home_path}/config/gpt35.yaml"
+    with open(llm_config_file, "r") as f:
+        config = yaml.load(f, Loader=yaml.FullLoader)
+        modelname = config["model"]
+        print(f"start generate {task} with {modelname}")
+    context_data_file = f"{home_path}/benchmark/{task}/test.jsonl"
+    output_path = f"{home_path}/output/generate/rougl/{modelname}_{task}.jsonl"
+    respones = f"""python collect_clean_response.py --seed {seed} --dataset_name {task} \
+        --llm_config_file {llm_config_file} \
+        --batch_size 20 --output_path {output_path} \
+        --context_data_file {context_data_file} \
+        --log_steps 10 --resume
+        """
+    os.system(respones)
+
+
+def process_task(task_list):
+    # 在进程内部初始化 generate
+    generate(task_list)
+
+
+def multi_process_template_model(task_list, num_processes=None):
+    if num_processes is None:
+        num_processes = multiprocessing.cpu_count()
+
+    with multiprocessing.Pool(processes=num_processes) as pool:
+        pool.map(process_task, task_list)
+
+
+if __name__ == "__main__":
+    task_list = [
+        "email",
+        "code",
+        "abstract",
+        "table",
+    ]
+    # generate)
+    multi_process_template_model(task_list)
+    # eval
